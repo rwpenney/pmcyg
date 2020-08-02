877b8cae9c5a173a5aa1e8883022017ce36e00d4
Richard Penney <rwp@rwpenney.uk>
2020-08-02 07:06:54 +0100

    Refactored application into Python module + small driver

---
+++ pmcyg/__init__.py
@@ -0,0 +1,22 @@
+"""
+pmcyg utilities
+"""
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+__all__ = [ 'core', 'version' ]
+
+from .core import DEFAULT_INSTALLER_URL, GarbageConfirmer, \
+                  HOST_IS_CYGWIN, PackageSet, PMbuilder
+from .version import PMCYG_VERSION
--- setup.py
+++ setup.py
@@ -4,54 +4,17 @@
 
 from distutils.core import setup
 import distutils.command.build_scripts as DIB
-import os, re
-
-pmcyg_scripts = [ 'pmcyg.py' ]
-
-
-class pmcyg_build_scripts(DIB.build_scripts):
-    """Helper class to strip filename suffixes when
-    installing Python scripts on POSIX platforms"""
-    def copy_scripts(self):
-        orig_scripts = self.scripts
-        tempfiles = []
-        if os.name == 'posix':
-            (self.scripts, tempfiles) = self._stripSuffixes(orig_scripts)
-        DIB.build_scripts.copy_scripts(self)
-        self.scripts = orig_scripts
-        for tmp in tempfiles:
-            os.remove(tmp)
-
-    def _stripSuffixes(self, orig_scripts):
-        """Remove .py suffix from Python scripts, e.g. for POSIX platforms"""
-        newnames = []
-        tempfiles = []
-
-        for script in orig_scripts:
-            newname = script
-            if script.endswith('.py'):
-                newname = script[:-3]
-                try:
-                    if os.path.exists(newname): raise IOError
-                    os.symlink(script, newname)
-                    tempfiles.append(newname)
-                except:
-                    newname = script
-            newnames.append(newname)
-
-        return (newnames, tempfiles)
+import os, re, shutil
+from pmcyg.version import PMCYG_VERSION
 
 
-
-# Extract version number from pmcyg.py script
-# NOTE that we cannot just 'from pmcyg import PMCYG_VERSION' because
-#   python3.x might reject the syntax of a python2.x script
-try:
-    PMCYG_VERSION = re.compile(r'^PMCYG_VERSION\s*=\s[\'"](.*)["\']$',
-                                re.MULTILINE) \
-                        .search(open('pmcyg.py', 'rt').read()).group(1)
-except:
-    PMCYG_VERSION = '0.x'
+if os.name == 'posix':
+    if not os.path.exists('build/scripts'):
+        os.makedirs('build/scripts')
+    shutil.copyfile('pmcyg.py', 'build/scripts/pmcyg')
+    pmcyg_scripts = [ 'build/scripts/pmcyg' ]
+else:
+    pmcyg_scripts = [ 'pmcyg.py' ]
 
 
 setup(
@@ -74,8 +37,8 @@ setup(
     download_url = 'https://github.com/rwpenney/pmcyg/archive/pmcyg-' \
                 + PMCYG_VERSION + '.tar.gz',
     version = PMCYG_VERSION,
+    packages = [ 'pmcyg' ],
     scripts = pmcyg_scripts,
-    cmdclass = { 'build_scripts': pmcyg_build_scripts },
     classifiers = [ 'Programming Language :: Python :: 3',
                     'Topic :: Utilities' ]
 )
